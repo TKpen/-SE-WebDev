804da911b92f51714a7b7ef8509652992c437605
Jayden Kruse <Jayden.Kruse@trojans.dsu.edu>
2025-11-06 16:45:34 -0600

    Added modularity feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Notes from "./components/Notes.jsx";
+import Dashboard from "./components/Dashboard.jsx";
 
 export default function App() {
   return (
     <div className="max-w-5xl mx-auto p-4 space-y-6">
       <Notes storageKey="campus.notes" />
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import GridLayout from "react-grid-layout";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+import Notes from "./Notes";
+
+export default function Dashboard() {
+    const layout = [
+        {i: "calendar", x: 0, y: 0, w: 6, h: 6},
+        {i: "notes", x: 6, y: 0, w: 6, h: 6},
+    ]
+
+    return (
+        <div className="min-h-screen bg-gray-50 p-6">
+            <h1 className="mb-4 text-xl font-bold text-white">Student Organizer Dashboard</h1>
+
+            <GridLayout
+                className="layout"
+                layout={layout}
+                cols={12}
+                rowHeight={30}
+                width={1200}
+                useCSSTransforms={true}
+            >
+                <div key="calendar" className="rounded-xl  bg-gray-500 text-white border-r-8 border-b-8 border-gray-700 p-4 shadow">
+                    <Notes />
+                </div>
+                <div key="notes" className="rounded-xl border bg-white p-4 shadow">
+                    <h2 className="font-semibold">Notes Module</h2>
+                    <p className="text-sm text-gray-500">Placeholder for notes</p>
+                </div>
+            </GridLayout>
+        </div>
+    )
+}
