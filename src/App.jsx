2157a3d41e430a82ef6820bd0b27651cdea5d288
eli_watchorn <eliwatchorn@gmail.com>
2025-11-06 16:43:23 -0600

    Todo list functionality (No CSS)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Notes from "./components/Notes.jsx";
+import ToDoList from "./components/Todo.jsx";
 
 export default function App() {
   return (
     <div className="max-w-5xl mx-auto p-4 space-y-6">
       <Notes storageKey="campus.notes" />
+      <ToDoList />
     </div>
   );
 }
