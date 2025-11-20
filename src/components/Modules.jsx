1cbecdd67d49b084f5b46448542c6535103f4a2d
Jayden Kruse <Jayden.Kruse@trojans.dsu.edu>
2025-11-20 13:25:05 -0600

    Added ToDo and GradeCalc components to modules component

--- src/components/Modules.jsx
+++ src/components/Modules.jsx
@@ -4,14 +4,20 @@ import Notes from "./Notes";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
 import "../styles/modules.tailwind.css"
+import GradeCal from "./GradeCal";
+import LinkList from "./LinkList";
+import ToDoList from "./Todo";
 
 const layout = [
-    { i: "notes", x:0, y:0, w:6, h:6, minH:4},
+    {i: "calendar", x: 0, y: 0, w: 6, h: 6},
+    {i: "notes", x: 6, y: 0, w: 6, h: 6, minH: 4},
+    {i: "todo", x: 6, y: 6, w: 4, h: 8, minH: 3, minW: 3},
 ];
 
 export default function Modules() {
     return (
-        <GridLayout
+        <div>
+            <GridLayout
                 className="layout"
                 layout={layout}
                 cols={12}
@@ -27,6 +33,44 @@ export default function Modules() {
                         <Notes />
                     </div>
                 </div>
+                <div 
+                    key="todo" 
+                    className="module-box flex flex-col h-full"
+                    style={{height: "100%"}}
+                >
+                    <div className="drag-box module-drag-handle flex-none sticky top-0 z-10">
+                        <h2 className="text-sm font-semibold p-2">To-Do</h2>
+                        <span className="text-xs text-gray-600 p-2">...</span>
+                    </div>
+                    <div className="flex-1 overflow-y-auto scrollbar-hide">
+                        <ToDoList />
+                    </div>
+                </div>
+                
             </GridLayout>
+
+            <GridLayout
+                className="layout"
+                layout={layout}
+                cols={12}
+                rowHeight={30}
+                width={1200}
+                draggableHandle=".module-drag-handle"
+            >
+                <div 
+                    key="notes" 
+                    className="module-box flex flex-col h-full"
+                    style={{height: "100%"}}
+                >
+                    <div className="drag-box module-drag-handle flex-none sticky top-0 z-10">
+                        <h2 className="text-sm font-semibold p-2">Grade Calculator</h2>
+                        <span className="text-xs text-gray-600 p-2">...</span>
+                    </div>
+                    <div className="flex-1 overflow-y-auto scrollbar-hide">
+                        <GradeCal />
+                    </div>
+                </div>
+            </GridLayout>
+        </div>
     )
 }
